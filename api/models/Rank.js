4cf41063e9baed550d0e7ef5658e78ab8e705c64
Ezequely <ezequelyfreitas@gmail.com>
2014-02-28 16:44:04 -0300

    create model Rank

---
+++ api/models/Rank.js
@@ -0,0 +1,26 @@
+/**
+ * Rank
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	
+  	/* e.g.
+  	nickname: 'string'
+  	*/
+    
+    name	: 'STRING',
+
+
+    pontuacao	: 'INTEGER',
+
+
+    game	: 'STRING'
+  }
+
+};
