38c0e1c60311a7eef626755e53a59489f78d13c9
Ezequely <ezequelyfreitas@gmail.com>
2014-03-10 15:49:27 -0300

    Bug do Angular ainda nao identificado

--- core.js
+++ core.js
@@ -4,7 +4,7 @@ function mainController($scope, $http) {
 	$scope.formData = {};
 
 	// when landing on the page, get all publications and show them
-	$http.get('http://istim-ranking.nodejitsu.com/rank')
+	$http.get('api/rank')
 		.success(function(data) {
 			$scope.pubs = data;
 			console.log(data);
